436470ab46cf663dbb853de2ae4f41e4e4e38a13
Ayan2027 <ayandhal2014@gmail.com>
2026-01-17 14:34:53 +0530

    add app.js to configure express app and middleware

---
+++ server/src/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+
+const healthRoutes = require("./routes/health.routes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/api/health", healthRoutes);
+
+module.exports = app;
